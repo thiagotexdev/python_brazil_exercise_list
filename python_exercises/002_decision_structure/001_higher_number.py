8e27d01f7d4853bc848bc7d41afce4d76e13297f
Thiago Jorge <thiago.texdev@gmail.com>
2022-07-13 16:39:50 -0300

    fix: fixing first decision exercise in the repository

--- python_exercises/002_decision_structure/001_higher_number.py
+++ python_exercises/002_decision_structure/001_higher_number.py
@@ -6,7 +6,7 @@ print(f'The number informed was: {number_2} ')
 if(number_1 > number_2):
     print(f'The higher number informed was: {number_1} ')
 elif(number_1 == number_2):
-    print(f'The number informed is equal !! ')
+    print('The number informed is equal !! ')
 else:
     print(f'The higher number informed was: {number_2} ')
 
