457cc0dd853fd4874a90b5c690430507e44e7ab3
Thiago Jorge <thiago.texdev@gmail.com>
2022-07-13 16:37:17 -0300

    chore: added first decision exercise in the repository

---
+++ python_exercises/002_decision_structure/001_higher_number.py
@@ -0,0 +1,14 @@
+number_1 = input('Enter a first number: ')
+print(f'The number informed was: {number_1} ')
+number_2 = input('Enter a second number: ')
+print(f'The number informed was: {number_2} ')
+
+if(number_1 > number_2):
+    print(f'The higher number informed was: {number_1} ')
+elif(number_1 == number_2):
+    print(f'The number informed is equal !! ')
+else:
+    print(f'The higher number informed was: {number_2} ')
+
+print(type(number_1))
+print(type(number_2))
